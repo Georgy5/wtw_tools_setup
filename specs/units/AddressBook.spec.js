53cc02d560d621c3a8e732587eed94d3d687bcf2
Karl Keller <kgk86@hotmail.com>
2021-09-01 21:07:14 +0200

    Test AddressBook.create

--- specs/units/AddressBook.spec.js
+++ specs/units/AddressBook.spec.js
@@ -6,4 +6,34 @@ describe("AddressBook", () => {
   it( () => is.expected.to.be.an("object"));
 
   it( () => is.expected.to.be.an.instanceof(AddressBook));
+
+  describe('#create', () => {
+    it(() => is.expected.to.respondTo('create'));
+    let setItemSpy
+    def('validData', {
+      name: 'Thomas',
+      email: 'thomas@somewhere.com',
+      phone: '031-111111',
+    })
+    before(() =>{
+      setItemSpy = sinon.spy(window.localStorage, 'setItem')
+    });
+
+    context('with valid data', () => {
+      def('contactsInStorage', () => JSON.parse(window.localStorage.getItem('contacts')));
+      def('response', () => $subject.creata($validData));
+
+      beforeEach(() => {
+        $response;
+      });
+
+      it('is expected to make use of localStorage.setItem', ()=> {
+        expect(setItemSpy).to.have.been.calledOnce;
+      });
+
+      it('is expected to store a contact', ()=> {
+        expect($contactsInStorage).to.have.lenght(1);
+      });
+    });
+  });
 });
