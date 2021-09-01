72377bf9323c74d04cbde40a63712c6689e5df76
Karl Keller <kgk86@hotmail.com>
2021-09-01 20:01:38 +0200

    Test localStorage.setItem

--- specs/units/localStorage.spec.js
+++ specs/units/localStorage.spec.js
@@ -2,10 +2,25 @@ const { should } = require("chai")
 
 describe('localStorage', () => {
 
-  subject(() => window.localStorage)
+  subject(() => window.localStorage);
 
   it(() => is.expected.to.respondTo('setItem'));
   it(() => is.expected.to.respondTo('getItem'));
   it(() => is.expected.to.respondTo('removeItem'));
   it(() => is.expected.to.respondTo('clear'));
+
+  describe('#setItem', () => {
+    
+    before(() =>{
+      $subject.setItem('foo', 'bar');
+    });
+
+    it('is expected to add the chosen key', ()=> {
+      expect($subject.data).to.haveOwnProperty('foo');
+    });
+
+    it('is expected to store the value in the chosen key', () => {
+      expect($subject.data).to.eql('bar');
+    });
+  })
 });
